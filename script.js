2828c5a2dd6f166159e03c7b315158b8d541e6df
mike <8876812+imustbreakyou@users.noreply.github.com>
2024-01-08 15:26:54 -0800

    completed initial Flexbox structure, ready for dynamic grid creation

---
+++ script.js
@@ -0,0 +1,17 @@
+
+// have a button that populates and centers a grid of 16x16 squares
+// button click
+
+    // create 16 x 16 squares 
+
+// NEXT 
+// Add a button that will create a popup to prompt user for number of squares
+// sketch container DOESNT CHANGE Sizes just the squares within
+// Input number into field with 100 as max 
+// input = n 
+//button now creates n x n div squares 
+
+// NEXT 
+// Set up hover class and hover functionality to turn a square black.... 
+// Set up a timer so that square slowly reverts 
+// Set up a gradient so the square darkens 10% on each hover and loses 10% for X seconds
