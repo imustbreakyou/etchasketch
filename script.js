4aff5f99d2e3e60737be83c5222b235ab3fade59
mike <8876812+imustbreakyou@users.noreply.github.com>
2024-01-09 15:01:39 -0800

    Finish assignment with grid resizing slider, 10% opacity increase, and clear grid button

--- script.js
+++ script.js
@@ -3,29 +3,42 @@ const create = document.querySelector('#create');
 const etchContainer = document.querySelector('#etch');
 const clearGridButton = document.querySelector('#clear-grid');
 let gridSize = 0;
-let fadeActive = false;
-let fadeValue = 0;
 
+// Mouse Down Functionality
+let isMouseDown = false;
 
-// Fade Slider
-
-const fadeSliderId = document.getElementById('fade-slider-id');
-const fadeSliderValue = document.getElementById('fade-slider-value');
-
-fadeSliderId.addEventListener('input', function () {
-   console.log('Fade slider value (during slide):', this.value);
-   delayInSeconds = (parseInt(this.value) / 1000).toFixed(1);
-   
-   fadeSliderValue.innerHTML = `${delayInSeconds} second delay` 
-
+document.addEventListener('mousedown', () => {
+    isMouseDown = true;
 });
 
-fadeSliderId.addEventListener('change', function () {
-   console.log('Fade Slider value (after release):', this.value);
+document.addEventListener('mouseup', () => {
+    isMouseDown = false;
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    createGrid(16); // Calls createGrid function with a default value of 16
+});
+
+// Fade Slider - Keeping it in comments to reference for future use
+
+//const fadeSliderId = document.getElementById('fade-slider-id');
+//const fadeSliderValue = document.getElementById('fade-slider-value');
+
+//fadeSliderId.addEventListener('input', function () {
+   //console.log('Fade slider value (during slide):', this.value);
+   //delayInSeconds = (parseInt(this.value) / 1000).toFixed(1);
+   
+   ////fadeSliderValue.innerHTML = `fade after ${delayInSeconds} seconds` 
+
+//});
+
+//fadeSliderId.addEventListener('change', function () {
+  //// console.log('Fade Slider value (after release):', this.value);
 
  
 
-});
+//});
 
 // Grid Slider
 
@@ -35,7 +48,7 @@ const gridSliderValue = document.getElementById('grid-slider-value');
 gridSliderId.addEventListener('input', function () {
     
     console.log('Grid Slider value (during slide):', this.value);
-    gridSliderValue.innerHTML = `${this.value} x ${this.value}`;
+    gridSliderValue.innerHTML = `'${this.value} x ${this.value}' grid`;
 
 
 });
@@ -62,64 +75,27 @@ clearGridButton.addEventListener('click', function () {
 function createGrid(gridInput) {
     etchContainer.innerHTML = '';
     let input = gridInput;
-        let text;
-        if (input === null || input > 100 || input <= 0 || input === ''|| isNaN(input)) {
-            text = "please enter a value between 0 and 100";
-            window.alert(text);
 
-        } else {
-            const n = parseInt(input);
-            etchContainer.style.setProperty('--n', n);
-            for (let i = 0; i < n ; i++) {
-                const row = document.createElement('div');
-                row.setAttribute('style', 'display: flex; flex: 1; padding: 0px; margin: 0px; ');
-                etchContainer.appendChild(row);
-        
-                for (let j = 0; j < n ; j++) {
-                    const square = document.createElement('div');
-                    square.classList.add('square')
-                    square.setAttribute('style', 'opacity: 0; background: black; padding-bottom: calc(100% / var(--n)); width: calc(100% / var(--n)); padding: 0px; margin: 0px;');
-                    square.addEventListener('mouseenter', () => {
-                        let currentOpacity = parseFloat(square.style.opacity) || 0;
-                        let newOpacity = currentOpacity + 0.1;
-                        square.style.opacity = newOpacity; 
-                        
-                    })
-                    square.addEventListener('mouseleave', () => {
-                        setTimeout(() => {
-                            square.style.opacity -= 0.1;
+    const n = parseInt(input);
+    etchContainer.style.setProperty('--n', n);
+    for (let i = 0; i < n ; i++) {
+        const row = document.createElement('div');
+        row.setAttribute('style', 'display: flex; flex: 1; padding: 0px; margin: 0px; ');
+        etchContainer.appendChild(row);
 
-                        }, 5000);
-                        
-                    
-                    })
-                    row.appendChild(square);
+        for (let j = 0; j < n ; j++) {
+            const square = document.createElement('div');
+            square.classList.add('square')
+            square.setAttribute('style', 'opacity: 0; background: black; padding-bottom: calc(100% / var(--n)); width: calc(100% / var(--n)); padding: 0px; margin: 0px;');
+            square.addEventListener('mouseenter', () => {
+                if (isMouseDown) {
+                    let currentOpacity = parseFloat(square.style.opacity) || 0;
+                    let newOpacity = currentOpacity + 0.1;
+                    square.style.opacity = newOpacity; 
                 }
-            }
+            });
+
+            row.appendChild(square);
         }
-        
-        };    
-
-
-
-
-   
-
-
-
-// have a button that populates and centers a grid of 16x16 squares
-// button click
-
-    // create 16 x 16 squares 
-
-// NEXT 
-// Add a button that will create a popup to prompt user for number of squares
-// sketch container DOESNT CHANGE Sizes just the squares within
-// Input number into field with 100 as max 
-// input = n 
-
-
-// NEXT 
-// Set up hover class and hover functionality to turn a square black.... 
-// Set up a timer so that square slowly reverts 
-// Set up a gradient so the square darkens 10% on each hover and loses 10% for X seconds
+    }
+}
